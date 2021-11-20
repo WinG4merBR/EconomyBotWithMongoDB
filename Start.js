39f12e3cb9424d2b3f29cdeedc27e77425e6961e
WinG4merBR <ddgom2334@outlook.com>
2021-11-20 20:14:35 -0300

    Start.js removido

--- Start.js
+++
@@ -1,7 +0,0 @@
-const Client = require("./src/Client");
-const client = new Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"] });
-const config = require("./config.json");
-
-client.loadCommands();
-client.loadEvents();
-client.login(config.token);
